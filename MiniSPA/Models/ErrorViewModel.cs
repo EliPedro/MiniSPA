d7e1ea13afaad69c1cee4738cdd29e443d9a2e83
eli <elipedrodos.santos@gmail.com>
2018-03-29 22:17:34 -0300

    Primeiro Commit

---
+++ MiniSPA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiniSPA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
